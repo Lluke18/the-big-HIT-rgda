fc463b51bc7c31b479a56b928d1c9b26635418b5
Dani Munteanu <daniel.munteanu@student.upt.ro>
2026-08-13 05:59:14 +0300

    Fixed vent tp bug, but players still can't exit the level and re-enter

--- scripts/level/main.gd
+++ scripts/level/main.gd
@@ -11,8 +11,10 @@ var werewolf_scene := preload("res://scenes/characters/character.tscn")
 func _ready() -> void:
 	if SignalBus.steam_activated:
 		network_manager.active_network_type = network_manager.MULTIPLAYER_NETWORK_TYPE.STEAM
+	
+	#Nu stiu daca ne mai trebuie
+	"""
 	if multiplayer.is_server():
-		
 		for peer_id in MultiplayerManager.player_characters:
 			var selection = MultiplayerManager.player_characters[peer_id]
 			var new_player: Node3D #maybe charbody3d or custom class!
@@ -26,6 +28,7 @@ func _ready() -> void:
 			new_player.add_to_group("character")
 			players_spawn.add_child(new_player, true)
 			#await get_tree().create_timer(3).timeout #ca sa nu se suprapuna
+	"""
 			
 """if OS.has_feature("dedicated_server"):
 		print("Starting dedicated server...")
